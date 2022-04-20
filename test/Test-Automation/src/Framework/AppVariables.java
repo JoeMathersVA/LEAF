be53bfb64fa7a3a453101daf63a5c6394742ffa3
MaxRichard <MaxRichard@host.docker.internal>
2022-04-19 19:53:42 -0500

    ChromeDriver updated

--- test/Test-Automation/src/Framework/AppVariables.java
+++ test/Test-Automation/src/Framework/AppVariables.java
@@ -7,10 +7,9 @@ public class AppVariables {
 
 
 	
-	public final static String CHROMEDRIVER = "C:\\DEV\\Tools\\Selenium\\ChromeDriver_98.0.4758.102\\chromedriver_win32\\chromedriver.exe";
+	public final static String CHROMEDRIVER = "C:\\DEV\\Tools\\Selenium\\Current_ChromeDriver\\chromedriver.exe";
+	//public final static String CHROMEDRIVER = "C:\\DEV\\Tools\\Selenium\\ChromeDriver_98.0.4758.102\\chromedriver_win32\\chromedriver.exe";
 	//public final static String CHROMEDRIVER = "C:\\DEV\\Tools\\Selenium\\ChromeDriver_96.0.4664.35\\chromedriver_win32\\chromedriver.exe";
-	//public final static String CHROMEDRIVER = "C:/DEV/Tools/Selenium/ChromeDriver_95.0.4638.17/chromedriver_win32/chromedriver.exe";
-	//public final static String CHROMEDRIVER = "C:/DEV/Tools/Selenium/ChromeDriver_94.0.4606.61/chromedriver_win32/chromedriver.exe";
 										
 	public final static String IEDRIVER = "C:/DEV/Tools/Selenium/IEDriver/IEDriverServer_Win32_3.150.1/IEDriverServer.exe";
 	
